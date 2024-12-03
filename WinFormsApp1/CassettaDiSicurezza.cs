046e6223d35c1762b5ef0459e75a534bfd8c596f
Shuai678 <ziyishuai0110@gmail.com>
2024-12-03 12:55:49 +0100

    classe oggetto, cassaforte

---
+++ WinFormsApp1/CassettaDiSicurezza.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class CassettaDiSicurezza
+    {
+        private List<OggettoSegreto> oggettiContenuti;
+        private string codiceUnivoco;
+        private string produttore;
+        private string codiceSegreto;
+        private bool vuoto;
+
+        public List<OggettoSegreto> OggettiContenuti
+        {
+            get { return oggettiContenuti; }
+        }
+        public CassettaDiSicurezza (string codiceUnivoco, string produttore, string codiceSegreto, bool vuoto)
+        {
+            this.codiceUnivoco = codiceUnivoco;
+            this.produttore = produttore;
+            this.codiceSegreto = codiceSegreto;
+            this.vuoto = vuoto;
+        }
+        public void Aggiungi(OggettoSegreto oggetto)
+        {
+            oggettiContenuti.Add(oggetto);
+        }
+
+        public void Rimuovi(OggettoSegreto oggetto)
+        {
+            if (oggetto == null)
+                return;
+            OggettiContenuti.Remove(oggetto);
+        }
+
+        public void ModificaPin (string codiceVecchio, string codiceNuovo)
+        {
+
+            if (codiceNuovo == null || codiceNuovo == codiceVecchio || codiceVecchio != codiceSegreto)
+                return;
+            codiceSegreto = codiceNuovo;
+        }
+
+
+    }
+}
---
+++ WinFormsApp1/OggettoSegreto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class OggettoSegreto
+    {
+        private string identificatore;
+        private string valore;
+        private double valoreAssicurato;
+        public OggettoSegreto (string identificatore, string valore)
+        {
+            this.identificatore = identificatore;
+            this.valore = valore;
+        }
+    }
+}
